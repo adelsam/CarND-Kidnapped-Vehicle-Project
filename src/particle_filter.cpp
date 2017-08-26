c7cc7343ffcd5c2f1276c8804de519f3160a4e36
adelman <samuel.adelman@gmail.com>
2017-08-26 07:50:20 -0400

    reduce particle count to meet time constraint

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -30,7 +30,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
   normal_distribution<double> dist_y(y, std[1]);
   normal_distribution<double> dist_theta(theta, std[2]);
 
-  num_particles = 1000;
+  num_particles = 100;
   for (int i = 0; i < num_particles; i++) {
     Particle p = Particle();
     p.id = i;
